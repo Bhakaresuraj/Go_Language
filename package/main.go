10865d9640e8c0c8edc9e417dd490b001cad0705
Bhakaresuraj <surajbhakare190@gmail.com>
2026-04-05 17:58:36 +0530

    Packages  -learrned what is package  -Learned how to use package  -learned os,color,strings package  -learned env variables

--- package/main.go
+++ package/main.go
@@ -1,14 +1,61 @@
 package main
-
+ 
 import (
+    "fmt"
+    "github.com/fatih/color"
+    "os"
+    "strings"
+    "strconv"
 
- "github.com/fatih/color"
- "fmt"
 )
 
 func main(){
 
-	color.Green("api is healthy")
-        color.Red("worker service is down")
-       fmt.Println("plain output")
+    fmt.Println("Hello this is Go program for learning package :")
+    color.Green("This is printed using color package")
+    color.Red("This is printed using color package")
+
+    // Welcome to  os package 
+    port:=8080  //create and assign  ,integer type
+
+    msg :=fmt.Sprintf("Port : %d",port)
+    fmt.Println(msg);
+
+    // os.Getenv function returns string value
+    portStr :=os.Getenv("MYAPP_PORT")
+    fmt.Println("Port : ",portStr)
+
+
+    // Use of string convert package
+
+    // strconv.Atoi()  returns two values one is result and another is error 
+
+    portInt ,err :=strconv.Atoi(portStr)
+
+    if err != nil {
+        fmt.Println("Error :",err)
+        os.Exit(1)
+    }
+
+    fmt.Println("Port Int  :",portInt)
+
+
+    //host variable
+
+    host:=os.Getenv("MYAPP_HOST")
+
+    fmt.Println("Host : ",host)
+    
+    // accept our service name
+
+    service :=os.Getenv("MYAPP_NAME")
+
+    fmt.Println("Name:",service)
+
+    // string manipulation
+
+    fmt.Println(strings.ToUpper(service))
+
+
+
 }
