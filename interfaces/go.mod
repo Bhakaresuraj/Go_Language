6742fbe95ea480053fa5a04fcf6a3b8223043cb8
Bhakaresuraj <surajbhakare190@gmail.com>
2026-04-06 18:18:41 +0530

    Interface  -learned what is interface and how to omplement it  -Who can use it

---
+++ interfaces/go.mod
@@ -0,0 +1,3 @@
+module github.com/Bhakaresuraj/Go_language/interfaces
+
+go 1.26.1
---
+++ interfaces/main.go
@@ -0,0 +1,42 @@
+package main
+import (
+    "fmt"
+    "time"
+)
+
+type Checker interface{
+    Check()
+} 
+
+type HttpService struct{
+    Name string
+    URL string
+    Healthy bool
+}
+
+func (h HttpService) Check(){
+    // logic that checks  if service is healthy or not on the basis of h.URL
+
+    fmt.Println("Checking if ",h.URL , "is HEalthy or not.....")
+    time.Sleep(3*time.Second)
+    h.Healthy =true
+
+    fmt.Printf("HTTP => %s ,%s , %v \n",h.Name,h.URL,h.Healthy)
+
+}
+
+func main(){
+    fmt.Println("Welcome to Interfaces .......")
+
+    service := HttpService {Name :"example.com",URL :"https://example.com",Healthy :false}
+    services := []Checker{
+             HttpService {Name :"example.com",URL :"https://example.com",Healthy :false},
+    }
+    for _,svc :=range services{
+            
+        svc.Check()
+    }
+
+    service.Check()
+
+}
