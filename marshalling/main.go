eacde1f8f70c82fa60e89019a02303bcca82f16b
Bhakaresuraj <surajbhakare190@gmail.com>
2026-05-19 18:52:06 +0530

    Perform Marshalling and Unmarshalling in go language

--- marshalling/main.go
+++ marshalling/main.go
@@ -1,41 +1,87 @@
 package main
+
 import (
-    "fmt"
-    "io"
-    "net/http"
-    "encoding/json"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 )
 
-type Student struct {
-    Name string   `json : "name"`
-    Marks int      `json : "marks"`
-    City string     `json :"city"`
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Pong")
+}
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is serving from Port :8080")
+	fmt.Println("Method : ", r.Method)
+	fmt.Println("Query :", r.URL.RawQuery)
+	fmt.Println("Path :", r.URL.Path)
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "You called  : %s  %s\n", r.Method, r.URL.Path)
 
 }
 
-
-func homeHandler(w http.ResponseWriter , r *http.Request){
-    
-    fmt.Println(r.Body)
-    body ,err :=io.ReadAll(r.Body)
-    if err!=nil{
-        fmt.Println(err)
-        return 
-    }
-    var stud Student
-    err = json.Unmarshal(body ,&stud)
-    if err != nil{
-        fmt.Println("error in unMarshalling data")
-    }
-    
-    fmt.Println(stud)
-    fmt.Fprintln(w ,"Response from http server ...")
+func statusReader(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, r.Body)
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("Error Reading Body :", err)
+	}
+	fmt.Fprintf(w, "Data in the form of Byte Stream : \n ", data)
+	fmt.Fprintf(w, "\nString Data :--\n", string(data))
 
 }
 
-func main(){
-    fmt.Println("Server us runnig on port :8080 ")
-    http.HandleFunc("/",homeHandler)
-    http.ListenAndServe(":8080",nil)
+type UserInfo struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Location string `json:"location"`
+}
+
+type Result struct {
+	Math int `json:"math"`
+	Mar  int `json:"mar"`
+	Eng  int `json:"eng"`
+}
+
+func login(i UserInfo) (Result, error) {
+	var result Result
+	if i.Username == "suraj" && i.Password == "Pass" {
+		result = Result{Math: 99, Mar: 50, Eng: 91}
+		return result, nil
+	}
+	return result, fmt.Errorf("Please login by  Valid User details....")
+}
+
+func Marshalling(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("Error while reading Body :", err)
+	}
+	var data UserInfo
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		fmt.Println("Error during Unmarshalling :", err)
+	}
+	result, err := login(data)
+	if err != nil {
+		w.WriteHeader(400)
+		fmt.Fprintln(w, err)
+		return
+	}
+
+	resultJson, err := json.Marshal(result)
+	w.WriteHeader(200)
+	fmt.Fprintln(w, string(resultJson))
+}
+
+func main() {
+	// http.HandleFunc("/", homeHandler)
+	// http.HandleFunc("/read", statusReader)
+	// http.HandleFunc("/ping", pingHandler)
+	http.HandleFunc("/result", Marshalling)
+	fmt.Println("Server is Running on Port :8080")
+	http.ListenAndServe(":8080", nil)
 
 }
+
